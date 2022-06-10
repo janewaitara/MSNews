c12f90b5c1ec1bcbcdeb512a289751e94818b6bc
Waitara <janewaitara99@gmail.com>
2022-06-10 07:39:03 +0300

    Created moshi converter instance

--- app/src/main/java/com/example/msnews/data/di/Modules.kt
+++ app/src/main/java/com/example/msnews/data/di/Modules.kt
@@ -2,11 +2,20 @@ package com.example.msnews.data.di
 
 import com.example.msnews.data.api.NewsApiService
 import com.example.msnews.data.utils.Constants.REST_BASE_URL
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 val networkModule = module {
+
+    //Moshi instance with Kotlin adapter factory that Retrofit will be using to parse JSON
+    single {
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
 
     // retrofit class
     single {
