7f60de0084a681de4acad3f7d48836f80b328852
Waitara <janewaitara99@gmail.com>
2022-06-10 07:34:01 +0300

    Created Api interface instance in koin

--- app/src/main/java/com/example/msnews/data/di/Modules.kt
+++ app/src/main/java/com/example/msnews/data/di/Modules.kt
@@ -1,5 +1,6 @@
 package com.example.msnews.data.di
 
+import com.example.msnews.data.api.NewsApiService
 import com.example.msnews.data.utils.Constants.REST_BASE_URL
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -7,10 +8,16 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 val networkModule = module {
 
+    // retrofit class
     single {
         Retrofit.Builder()
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(get()))
             .baseUrl(REST_BASE_URL)
             .build()
+    }
+
+    // create NewsRemoteApi
+    single {
+        get<Retrofit>().create(NewsApiService::class.java)
     }
 }
