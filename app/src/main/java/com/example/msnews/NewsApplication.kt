e0d5f0845eae0b5e2c59d8337a7f1e6ab2cc682d
Waitara <janewaitara99@gmail.com>
2022-06-11 20:03:10 +0300

    Initialized koin

--- app/src/main/java/com/example/msnews/NewsApplication.kt
+++ app/src/main/java/com/example/msnews/NewsApplication.kt
@@ -4,12 +4,12 @@ import android.app.Application
 import com.example.msnews.data.di.apiModule
 import com.example.msnews.data.di.networkModule
 import com.example.msnews.data.di.repositoryModule
-import org.koin.core.logger.Level
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
-//Initialize koin when the app is created
+// Initialize koin when the app is created
 class NewsApplication : Application() {
 
     override fun onCreate() {
@@ -20,7 +20,7 @@ class NewsApplication : Application() {
     private fun initKoin() {
         startKoin {
             androidLogger(Level.DEBUG)
-            androidContext(this@NewsApplication) //injects the application context with koin
+            androidContext(this@NewsApplication) // injects the application context with koin
 
             modules(
                 listOf(
@@ -31,5 +31,4 @@ class NewsApplication : Application() {
             )
         }
     }
-
 }
