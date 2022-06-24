61b1a37d5a262ded73f005fd91a6d9a79f4f659a
Waitara <janewaitara99@gmail.com>
2022-06-24 08:01:50 +0300

    Added room entity

--- app/src/main/java/com/example/msnews/data/model/Article.kt
+++ app/src/main/java/com/example/msnews/data/model/Article.kt
@@ -1,6 +1,11 @@
 package com.example.msnews.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles_table")
 data class Article(
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val source: Source,
     val author: String?,
     val title: String?,
