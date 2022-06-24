35fdc452c66840281d9937b140f9d04a5c3a9cc5
Waitara <janewaitara99@gmail.com>
2022-06-24 09:22:32 +0300

    Added a typeConverter for Source class

--- app/src/main/java/com/example/msnews/data/db/ArticlesDatabase.kt
+++ app/src/main/java/com/example/msnews/data/db/ArticlesDatabase.kt
@@ -2,9 +2,11 @@ package com.example.msnews.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.msnews.data.model.Article
 
 @Database(entities = [Article::class], version = 1, exportSchema = false)
+@TypeConverters(SourceConverter::class)
 abstract class ArticlesDatabase : RoomDatabase() {
 
     abstract fun articlesDao(): ArticlesDao
---
+++ app/src/main/java/com/example/msnews/data/db/SourceConverter.kt
@@ -0,0 +1,20 @@
+package com.example.msnews.data.db
+
+import androidx.room.TypeConverter
+import com.example.msnews.data.model.Source
+
+/**
+ * Type converters are methods that tell Room how to convert custom types to and from known types
+ * that Room can persist
+ * */
+class SourceConverter {
+    @TypeConverter
+    fun fromSource(source: Source): String {
+        return "${source.id} : ${source.name}"
+    }
+
+    @TypeConverter
+    fun toSource(string: String): Source {
+        return Source(id = null, name = string)
+    }
+}
