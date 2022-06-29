b58fdb62ca5056864fb04bb148d3448ffa4396c4
Waitara <janewaitara99@gmail.com>
2022-06-29 23:52:04 +0300

    Added logic to separate data sources based on category

--- app/src/main/java/com/example/msnews/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/msnews/viewmodels/NewsViewModel.kt
@@ -45,15 +45,30 @@ class NewsViewModel(
         viewModelScope.launch {
             _status.value = Resource.Loading()
             try {
-                // val apiResponse = newsRepository.getTopHeadlinesFromApi(category, language).data!!
                 val apiResponse = newsRepository.getNewsFromDb()
                 apiResponse.collect { listOfArticles ->
                     Log.d("Data fetched", listOfArticles.size.toString())
                     _listOfTopArticles.value = listOfArticles
                     _status.value = Resource.Success(listOfArticles)
                 }
-                // _listOfTopArticles.value = apiResponse.
-                // _status.value = Resource.Success(apiResponse.value!!)
+            } catch (e: Exception) {
+                _status.value = Resource.Error(e.localizedMessage)
+                _listOfTopArticles.value = listOf()
+            }
+        }
+    }
+
+    fun getTopHeadlinesFromAPI(
+        category: String,
+        language: String
+    ) {
+        viewModelScope.launch {
+            _status.value = Resource.Loading()
+            try {
+                val apiResponse = newsRepository.getTopHeadlinesFromApi(category, language).data!!
+                Log.d("Data fetched for $category", apiResponse.articles.size.toString())
+                _listOfTopArticles.value = apiResponse.articles
+                _status.value = Resource.Success(apiResponse.articles!!)
             } catch (e: Exception) {
                 _status.value = Resource.Error(e.localizedMessage)
                 _listOfTopArticles.value = listOf()
@@ -66,6 +81,7 @@ class NewsViewModel(
             _status.value = Resource.Loading()
             try {
                 val apiResponse = newsRepository.getSearchedNews(searchQuery, language).data!!
+                Log.d("Data fetched for $searchQuery", apiResponse.articles.size.toString())
                 _listOfSearchedArticles.value = apiResponse.articles
                 _status.value = Resource.Success(apiResponse.articles)
             } catch (e: Exception) {
@@ -75,13 +91,28 @@ class NewsViewModel(
         }
     }
 
+    /**
+     * Dilemma: I'm fetching categorised data whose response does not have categorisation in it
+     * hence on room, all categories are stored together. This means that fetching from the db will
+     * return the same list regardless of the chip(category) selected.
+     * To solve this: In the meantime, I'm only storing general data in room
+     * while others I fetch from the network hence the when statement.
+     * */
     fun setCategory(selectedCategory: String) {
         _categoryFilter.value = selectedCategory
-        getNewsFromApiAndInsertIntoDb(selectedCategory, "en")
-        getTopHeadlinesFromDb()
+
+        when (selectedCategory) {
+            "General" -> {
+                getNewsFromApiAndInsertIntoDb(selectedCategory, "en")
+                getTopHeadlinesFromDb()
+            }
+            else -> {
+                getTopHeadlinesFromAPI(selectedCategory, "en")
+            }
+        }
     }
 
-    fun hasNoCategorySet(): Boolean {
+    private fun hasNoCategorySet(): Boolean {
         return _categoryFilter.value.isNullOrEmpty()
     }
 
