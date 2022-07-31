84674cbff56c23dbe49afb6d5a8bb29adf3bc7ea
Waitara <janewaitara99@gmail.com>
2022-07-31 22:50:31 +0300

    Updated ArticlesDao to return a pagingSource for the News

--- app/src/main/java/com/example/msnews/data/db/ArticlesDao.kt
+++ app/src/main/java/com/example/msnews/data/db/ArticlesDao.kt
@@ -1,11 +1,11 @@
 package com.example.msnews.data.db
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.msnews.data.model.Article
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ArticlesDao {
@@ -13,8 +13,9 @@ interface ArticlesDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertNews(articles: List<Article>)
 
+    // That way, the articles table becomes the source of data for Paging.
     @Query("SELECT * from articles_table")
-    fun getNews(): Flow<List<Article>>
+    fun getNews(): PagingSource<Int, Article>
 
     @Query("DELETE FROM articles_table")
     suspend fun clearNews()
